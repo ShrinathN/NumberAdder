1e44aa1dfe5105ea7ffbd1b9bdc982bf190337de
Shrinath Nimare <shrinath.nimare@gmail.com>
2019-03-05 14:31:14 +0530

    Fixed bugs

--- numadd.py
+++ numadd.py
@@ -8,7 +8,7 @@ def android_input_touch(xy):
 	call('adb shell input touchscreen tap ' + xy, shell=True)
 
 def android_input_text(txt):
-	call('adb shell input text ' + txt, shell=True)
+	call('adb shell input text "\'' + txt +  '\'"' , shell=True)
 	
 def android_input_swipe(xy1, xy2, milli):
 	call('adb shell input touchscreen swipe ' + xy1 + " " + xy2 + " " + milli, shell=True)
@@ -33,10 +33,11 @@ for i in range(0, len(data)):
 	android_input_touch(point_new_contact_xy)
 	android_input_text(first_name_tag)
 	android_input_touch(point_last_name_xy)
-	android_input_text(data['Name'][i])
-	android_input_swipe(point1_swipe_down, point2_swipe_down, 500)
+	android_input_text(str(data['Name'][i]))
+	android_input_swipe(point1_swipe_down, point2_swipe_down, '500')
 	android_input_touch(point_number_xy)
 	android_input_text(str(data['Number'][i]))
 	android_input_touch(point_submit_xy)
+	android_input_swipe("570 400", "570 1400", "200")
 	time.sleep(1)
 	print('Done')
